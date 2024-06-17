e9ed5fd191b2e1d4a88a05a1b9400ac59835de84
Jagadish Rangrej <jrangrej@hotmail.com>
2024-06-16 23:36:13 -0400

    Delete include/wifidata.h

--- include/wifidata.h
+++
@@ -1,6 +0,0 @@
-// // Replace with your network credentials
-const char* ssid     = "SPARTANET";
-const char* password = "ByuQ93&2t";
-
-// const char* ssid     = "Jrphone";
-// const char* password = "watersoup123";
